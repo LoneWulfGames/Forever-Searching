2577cca1be14194d5e2840e6e63b4c4b1208ddcd
LoneWulfGames <adamb@biastudios.com>
2018-05-13 21:28:59 -0400

    Created Mob Scriptable Object
    Includes all planned races.

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public enum Races {
+    TERR, DWARF, HALFLING, GNOME,
+    OGRE, TROLL, GOBLIN, GREMLIN,
+    LIGHTELF, WOODELF, SHADOWELF, PIXIE,
+    KOTTORIC, VARANUS, URSINAE, MINOTAUR,
+    REDDRACONIAN, BLUEDRACONIAN, GREENDRACONIAN, GOLDDRACONIAN };
+        
+[CreateAssetMenu(fileName = "New Mob", menuName = "Mob")]
+public class Mob: ScriptableObject  {
+
+    public new string name;
+    public Races race;
+
+    public int health;
+
+    public int strength;
+    public int intelligence;
+    public int dexterity;
+
+
+}
