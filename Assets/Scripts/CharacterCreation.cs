1e4cc16d944dc0e7e82dfa4505aa4ccd98a68d12
Adam Barse <adamb@biastudios.com>
2018-05-16 15:04:39 -0400

    Improved Menu
    More buttons!
    Stats!

--- Assets/Scripts/CharacterCreation.cs
+++ Assets/Scripts/CharacterCreation.cs
@@ -5,14 +5,28 @@ using UnityEngine.UI;
 
 public class CharacterCreation : MonoBehaviour {
 
+    [SerializeField]
+    private float STRFillAmount;
+    [SerializeField]
+    private float INTFillAmount;
+    [SerializeField]
+    private float DEXFillAmount;
+    [SerializeField]
+    private Image STRContent;
+    [SerializeField]
+    private Image INTContent;
+    [SerializeField]
+    private Image DEXContent;
+
+
     public Mob mob;
     GameObject selectedRace;
     public Text descText;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         ShowMob();
-	}
-	
+    }
+
 
     public void SwapMob(Mob newMob)
     {
@@ -26,5 +40,20 @@ public class CharacterCreation : MonoBehaviour {
         selectedRace = Instantiate(mob.body, transform);
         transform.position = Vector3.up * (selectedRace.transform.localScale.y - 1);
         descText.text = mob.description;
+        HandleStatBars();
+    }
+
+    void HandleStatBars()
+    {
+        STRContent.fillAmount = Map(mob.strength, 0,20, 0, 1);
+
+        INTContent.fillAmount = Map(mob.intelligence, 0, 20, 0, 1);
+
+        DEXContent.fillAmount = Map(mob.dexterity, 0, 20, 0, 1);
+
+    }
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 }
